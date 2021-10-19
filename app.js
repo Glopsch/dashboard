fedc0a3bf16f20f50965b84dcfbef2749d478166
glopsch <leon.hugr@gmail.com>
2021-10-19 11:21:25 +0200

    Optimize nmap requests; Add ports scan; Set up authentication handling (BE)

--- app.js
+++ app.js
@@ -1,22 +1,40 @@
-const express = require('express');
+const express     = require('express');
 
-const scan = require( './controllers/scanGet' );
+const scan              = require( './controllers/scanGet' );
+const auth              = require( './controllers/auth' );
+
+const { requireAuth }   = require( './middleware/requireAuth' );
 
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
 app.use(express.static('public'));
 
 
-app.get('/scan/network/:ip/:suffix', (req, res) => {
+app.get('/scan/network/:ip/:suffix', requireAuth, (req, res) => {
   scan.handleNetworkScanGet(req, res);
 });
-
-app.get('/scan/detail/:ip', (req, res) => {
+app.get('/scan/ports/:ip', requireAuth, (req, res) => {
+  scan.handlePortScanGet(req, res);
+});
+app.get('/scan/detail/:ip', requireAuth, (req, res) => {
   scan.handleDetailScanGet(req, res);
 });
 
 
+app.post('/auth/init', (req, res) => {
+  auth.handleInitPost(req, res);
+})
+app.post('/auth/login', (req, res) => {
+  auth.handleLoginPost(req, res);
+})
+
+
 app.listen(port, () => {
-  console.log(`Dashboard app listening at http://localhost:${port}`)
+  console.log(`Dashboard app listening on port ${port}`)
 });
--- controllers/scanGet.js
+++ controllers/scanGet.js
@@ -4,7 +4,7 @@ nmap.nmapLocation = 'C:\\Program Files (x86)\\Nmap\\nmap.exe'; // "nmap"; //defa
 
 function handleNetworkScanGet(req, res) {
     //    Accepts array or comma separarted string for custom nmap commands in the second argument.
-    var nmapscan = new nmap.NmapScan(`${req.params.ip}/${req.params.suffix}`, '-sn -R'); //-sn
+    var nmapscan = new nmap.NmapScan(`${req.params.ip}/${req.params.suffix}`, '-sn -R -T5'); //-sn
     
     nmapscan.on('complete',function(data){
         console.log(nmapscan.scanTime);
@@ -18,32 +18,29 @@ function handleNetworkScanGet(req, res) {
     });
     
     nmapscan.startScan();
-    // returns
-    // [  
-    //    {  
-    //       "hostname":"localhost",
-    //       "ip":"127.0.0.1",
-    //       "mac":null,
-    //       "openPorts":[  
+}
+
+function handlePortScanGet(req, res) {
+    //    Accepts array or comma separarted string for custom nmap commands in the second argument.
+    var nmapscan = new nmap.NmapScan(`${req.params.ip}`, '-sT -Pn -T5'); //-sn
     
-    //       ],
-    //       "osNmap":null
-    //    },
-    //    {  
-    //       "hostname":"google.com",
-    //       "ip":"74.125.21.113",
-    //       "mac":null,
-    //       "openPorts":[  
+    nmapscan.on('complete',function(data){
+        console.log(nmapscan.scanTime);
+        console.log(data);
+        res.send(data);
+    });
+
+    nmapscan.on('error', function(error){
+        console.log(error);
+        res.status( 500 );
+    });
     
-    //       ],
-    //       "osNmap":null
-    //    }
-    // ]
+    nmapscan.startScan();
 }
 
 function handleDetailScanGet(req, res) {
     //    Accepts array or comma separarted string for custom nmap commands in the second argument.
-    var nmapscan = new nmap.NmapScan(`${req.params.ip}`, '-sV -O'); //-sn
+    var nmapscan = new nmap.NmapScan(`${req.params.ip}`, '-sV -Pn -T5'); //-sn
     
     nmapscan.on('complete',function(data){
         console.log(nmapscan.scanTime);
@@ -57,151 +54,10 @@ function handleDetailScanGet(req, res) {
     });
     
     nmapscan.startScan();
-    // returns
-    // [  
-    //    {  
-    //       "hostname":"localhost",
-    //       "ip":"127.0.0.1",
-    //       "mac":null,
-    //       "openPorts":[  
-    
-    //       ],
-    //       "osNmap":null
-    //    },
-    //    {  
-    //       "hostname":"google.com",
-    //       "ip":"74.125.21.113",
-    //       "mac":null,
-    //       "openPorts":[  
-    
-    //       ],
-    //       "osNmap":null
-    //    }
-    // ]
-}
-
-
-
-function handleQuickScanGet(req, res) {
-
-    console.log(req.params.ip);
-    //    Accepts array or comma separated string of NMAP acceptable hosts
-    var quickscan = new nmap.QuickScan(`${req.params.ip}`);
-    
-    quickscan.on('complete', function(data){
-      console.log(data);
-      res.send(data);
-    });
-    
-    quickscan.on('error', function(error){
-      console.log(error);
-      res.status( 500 );
-    });
-    
-    quickscan.startScan();
-    // returns
-    // [  
-    //    {  
-    //       "hostname":"localhost",
-    //       "ip":"127.0.0.1",
-    //       "mac":null,
-    //       "openPorts":[  
-    
-    //       ],
-    //       "osNmap":null
-    //    },
-    //    {  
-    //       "hostname":"google.com",
-    //       "ip":"74.125.21.113",
-    //       "mac":null,
-    //       "openPorts":[  
-    
-    //       ],
-    //       "osNmap":null
-    //    }
-    // ]
-}
-
-function handleCustomScanGet(req, res) {
-    //    Accepts array or comma separarted string for custom nmap commands in the second argument.
-    var nmapscan = new nmap.NmapScan(`192.168.1.0/24`, '-sn -R'); //-sn
-    
-    nmapscan.on('complete',function(data){
-        console.log(nmapscan.scanTime);
-      console.log(data);
-      res.send(data);
-    });
-
-    nmapscan.on('error', function(error){
-      console.log(error);
-      res.status( 500 );
-    });
-    
-    nmapscan.startScan();
-    // returns
-    // [  
-    //    {  
-    //       "hostname":"localhost",
-    //       "ip":"127.0.0.1",
-    //       "mac":null,
-    //       "openPorts":[  
-    
-    //       ],
-    //       "osNmap":null
-    //    },
-    //    {  
-    //       "hostname":"google.com",
-    //       "ip":"74.125.21.113",
-    //       "mac":null,
-    //       "openPorts":[  
-    
-    //       ],
-    //       "osNmap":null
-    //    }
-    // ]
-}
-
-
-
-function handleOsAndPortScanGet(req, res) {
-    var osandports = new nmap.OsAndPortScan(`${req.params.ip}`);
-
-    osandports.on('complete',function(data){
-    console.log(data);
-    res.send(data);
-    });
-
-    osandports.on('error', function(error){
-    console.log(error);
-    res.status( 500 );
-    });
-
-    osandports.startScan();
-    // returns
-    // [
-    //    {  
-    //       "hostname":"google.com",
-    //       "ip":"74.125.21.113",
-    //       "mac":null,
-    //       "openPorts":[  
-    //          {  
-    //             "port":80,
-    //             "service":"http"
-    //          },
-    //          {  
-    //             "port":443,
-    //             "service":"https"
-    //          }
-    //       ],
-    //       "osNmap":"OpenBSD 4.3"
-    //    }
-    // ]
 }
 
 module.exports = {
     handleNetworkScanGet,
-    handleDetailScanGet,
-    handleQuickScanGet,
-    handleCustomScanGet,
-    handleOsAndPortScanGet
+    handlePortScanGet,
+    handleDetailScanGet
 };
