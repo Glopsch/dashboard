a522f8a4ac21a63f70fb89e866b09bb748c5ebdc
glopsch <leon.hugr@gmail.com>
2021-10-24 21:51:15 +0200

    Add automated network ip check

--- controllers/scanGet.js
+++ controllers/scanGet.js
@@ -1,14 +1,35 @@
 var nmap = require('node-nmap');
+var ip = require('ip');
 
+const { exec } = require('child_process');
 
 nmap.nmapLocation = process.env.NMAP_LOCATION || "nmap"; // "nmap"; //default
 
-const networkIP     = "192.168.2.0";
-const networkSuffix = "24";
+
+
+// const networkIP     = "192.168.2.0";
+// const networkSuffix = "24";
+
+function getNetworkAddressAndSuffix() {
+    exec("ip -o -f inet addr show | awk '/scope global/ {print $4}'", (err, localIP, stderr) => {
+        if (err) {
+            console.error(err)
+            return undefined;
+        } else {
+            console.log(`stdout: ${localIP}`);
+            return { networkAddress, subnetMaskLength } = ip.cidrSubnet(localIP);
+        }
+    });
+}
 
 function handleNetworkScanGet(req, res) {
+    const { networkAddress, subnetMaskLength } = getNetworkAddressAndSuffix();
+    console.log(networkAddress, subnetMaskLength);
+    // if( networkAddress == undefined || subnetMaskLength == undefined)
+    //     res.status(500);
+
     //    Accepts array or comma separarted string for custom nmap commands in the second argument.
-    var nmapscan = new nmap.NmapScan(`${networkIP}/${networkSuffix}`, '-sn -R -T5'); //-sn
+    var nmapscan = new nmap.NmapScan(`${networkAddress}/${subnetMaskLength}`, '-sn -R -T5'); //-sn
     
     nmapscan.on('complete',function(data){
         console.log(nmapscan.scanTime);
