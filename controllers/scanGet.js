ac846ad506670183eec7d143fe95943cff22b2ba
glopsch <leon.hugr@gmail.com>
2021-10-26 13:01:18 +0200

    Fix automated network check

--- controllers/scanGet.js
+++ controllers/scanGet.js
@@ -1,47 +1,47 @@
+require('dotenv').config();
+
 var nmap = require('node-nmap');
 var ip = require('ip');
 
-const { exec } = require('child_process');
+const { execSync } = require('child_process');
 
 nmap.nmapLocation = process.env.NMAP_LOCATION || "nmap"; // "nmap"; //default
 
-const fallbackNetworkIP     = "192.168.2.0";
-const fallbackNetworkSuffix = "24";
+const fallbackNetworkCIDR = process.env.FALLBACK_NETWORK_CIDR || "192.168.2.0/24";
 
 
 function getNetworkAddressAndSuffix() {
-    exec("ip -o -f inet addr show | awk '/scope global/ {print $4}'", (err, localIP, stderr) => {
-        if (err) {
-            console.log(err)
-            return undefined;
-        } else {
-            console.log(`stdout: ${localIP}`);
-            const { networkAddress, subnetMaskLength } = ip.cidrSubnet(localIP);
-            return { networkAddress, subnetMaskLength };
-        }
-    });
+    console.log(1);
+    try{
+        execSync("ip -o -f inet addr show | awk '/scope global/ {print $4}'", (err, localIP, stderr) => {
+            console.log(2);
+            if (err) {
+                console.log(err);
+                console.log(2.1);
+            } else {
+                console.log(`stdout: ${localIP}`);
+                const { networkAddress, subnetMaskLength } = ip.cidrSubnet(localIP);
+                console.log(2.2);
+                return `${networkAddress}/${subnetMaskLength}`;
+            }
+        });
+    } catch(err) {
+        console.log(3);
+        console.log("exec failed");
+        console.log(err);
+    }
+    console.log(4);
+    // const { networkAddress, subnetMaskLength } = ip.cidrSubnet("192.168.2.0/24");
+    // return `${networkAddress}/${subnetMaskLength}`;
+    return fallbackNetworkCIDR;
 }
 
 function handleNetworkScanGet(req, res) {
     const netIpAndSuffix = getNetworkAddressAndSuffix();
-    let networkAddress, subnetMaskLength;
-    if(!netIpAndSuffix) {
-        networkAddress      = fallbackNetworkIP;
-        subnetMaskLength    = fallbackNetworkSuffix;
-    } else {
-        if( !netIpAndSuffix.networkAddress || !netIpAndSuffix.subnetMaskLength) {
-            networkAddress      = fallbackNetworkIP;
-            subnetMaskLength    = fallbackNetworkSuffix;
-        } else {
-            networkAddress      = netIpAndSuffix.networkAddress;
-            subnetMaskLength    = netIpAndSuffix.subnetMaskLength;
-        }
-    }
-
-    console.log(networkAddress, subnetMaskLength);
+    console.log(netIpAndSuffix);
 
     //    Accepts array or comma separarted string for custom nmap commands in the second argument.
-    var nmapscan = new nmap.NmapScan(`${networkAddress}/${subnetMaskLength}`, '-sn -R -T5'); //-sn
+    var nmapscan = new nmap.NmapScan(netIpAndSuffix, '-sn -R -T5'); //-sn
     
     nmapscan.on('complete',function(data){
         console.log(nmapscan.scanTime);
